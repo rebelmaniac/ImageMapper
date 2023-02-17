8ab197546632724faf4d6c86dc46267b6c54069b
Brandon Kesner <77083072+rebelmaniac@users.noreply.github.com>
2023-02-16 22:02:57 -0500

    Display Specific routes that are housed in the JSON file. This is using the ImageMapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Container as BootstrapContainer } from "react-bootstrap";
 import DisplayRoute from "./components/DisplayRoute";
 import HighlightAreas from "./components/HighlightAreas";
 import JSON from "./data/area.json"
@@ -17,22 +16,17 @@ export const RouteContext = React.createContext();
 function App() {
 
   return (
-    <BootstrapContainer
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
+      <div>
         <RouteContext.Provider value={wall}>
           <Router>
             <Routes>
-              <Route path="/" element={<DisplayRoute wall={wall}/>}></Route>
-              <Route path="/RouteDisplay" element={<HighlightAreas wall={wall}/>}></Route>
+              <Route path="/" element={<DisplayRoute />}></Route>
+              <Route path="/RouteDisplay" element={<HighlightAreas />}></Route>
             </Routes>
           </Router>
 
         </RouteContext.Provider>
       </div>
-    </BootstrapContainer>
   );
 }
 
