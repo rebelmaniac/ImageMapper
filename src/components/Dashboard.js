37230d20deb387ca53b3129632516f163a0cdcaf
Brandon Kesner <77083072+rebelmaniac@users.noreply.github.com>
2023-04-11 18:49:16 -0400

    Clean up Files that are unecessary

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,6 @@ import React, { useState } from "react";
 import { Card, Alert, Button } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import { adaAddDoc } from "./add_ada_lovelace";
-import { addAlanTuring } from "./add_alan_turing";
-import { UserGrab } from "./get_all_users";
 
 export default function Dashboard() {
   const [error, setError] = useState("");
@@ -21,22 +18,6 @@ export default function Dashboard() {
           <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
             Update Profile
           </Link>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Add Users</h2>
-          <Button variant="info w-100 mt-3" onClick={() => adaAddDoc()}>
-            Add Ada
-          </Button>
-          <Button variant="info w-100 mt-3" onClick={() => addAlanTuring()}>
-            Add Alan
-          </Button>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Body>
-          <UserGrab />
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-3">
